9af941940675f1d09eabff266dec4cad18549f99
Nikolai Kochetkov <professor3222@gmail.com>
2024-12-23 12:34:23 +0000

    adding working deepspeech model, verified on one batch test

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,4 @@
 from src.model.baseline_model import BaselineModel
 from src.model.deepspeech_model import DeepSpeech
 
-__all__ = [
-    "BaselineModel",
-    'DeepSpeech'
-]
+__all__ = ["BaselineModel", "DeepSpeech"]
